04c6541d58b3c711c4e7ef881cb69ab9b01d2de6
YeasarArefin <yeasararefin007@gmail.com>
2023-05-24 16:08:15 +0600

    income and expenditure calculation by month done

--- source/v1/routes/dashboard.js
+++ source/v1/routes/dashboard.js
@@ -14,13 +14,46 @@ dashboard
 			const expenditureData = await expenditureCollection.find({});
 			const numberOfStudents = await accountsCollection.countDocuments({ role: "student" });
 			const numberOfTeachers = await accountsCollection.countDocuments({ role: "teacher" });
+
+			const monthlyIncome = await invoiceCollection.aggregate([
+				{
+					$group: {
+						_id: { $dateToString: { format: '%Y-%m', date: '$createdAt' } }, // Group by month
+						totalPayment: { $sum: '$payment' } // Calculate sum of payments
+					}
+				},
+				{
+					$project: {
+						_id: 0, // Exclude the default _id field
+						month: '$_id',
+						totalPayment: 1
+					}
+				}
+			]).exec();
+
+			const monthlyExpenditure = await expenditureCollection.aggregate([
+				{
+					$group: {
+						_id: { $dateToString: { format: '%Y-%m', date: '$createdAt' } }, // Group by month
+						amount: { $sum: '$amount' } // Calculate sum of payments
+					}
+				},
+				{
+					$project: {
+						_id: 0, // Exclude the default _id field
+						month: '$_id',
+						amount: 1
+					}
+				}
+			]).exec();
+
 			for (const { payment } of invoiceData) {
 				totalIncome += payment;
 			}
 			for (const { amount } of expenditureData) {
 				totalExpenditure += amount;
 			}
-			res.status(200).json({ income: totalIncome, expenditure: totalExpenditure, net: totalIncome - totalExpenditure, teachers: numberOfTeachers, students: numberOfStudents });
+			res.status(200).json({ income: totalIncome, monthlyIncome, expenditure: totalExpenditure, monthlyExpenditure, net: totalIncome - totalExpenditure, teachers: numberOfTeachers, students: numberOfStudents });
 		} catch (error) {
 			res.status(500).json({ error: error });
 		}
